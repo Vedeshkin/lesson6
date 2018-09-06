4ff99a781ff7fa834d02ba6d2f04e2172c527910
vedeshkin <evilfunt@gmail.com>
2018-09-07 00:40:05 +0300

    Simple chat client implemented

--- src/com/github/vedeshkin/Client.java
+++ src/com/github/vedeshkin/Client.java
@@ -1,7 +1,10 @@
 package com.github.vedeshkin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.Reader;
+import java.net.Socket;
 import java.util.Scanner;
 
 /**
@@ -11,10 +14,73 @@ public class Client {
 
     private static final int SERVER_PORT = 8089;
     private static final String HOST = "localhost";
+    private static final String STOP_STRING = "bye";
     private Scanner in;
     private PrintWriter out;
+    private static Client instance;
 
-    static void init(){}
+    private Client(){}
+
+    public static void main(String[] args) {
+        Client client = Client.getInstance();
+        client.init();
+
+    }
+
+    private static Client getInstance() {
+        if(instance == null) instance = new Client();
+        return instance;
+    }
+
+    private void init(){
+
+        Socket clientSocket;
+        try {
+            clientSocket = new Socket(HOST,SERVER_PORT);
+            in = new Scanner(clientSocket.getInputStream());
+            out = new PrintWriter(clientSocket.getOutputStream());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Thread t1 = new Thread(()->{
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+
+                while (true) {
+                    String message = br.readLine();
+                    if (!message.equals(STOP_STRING)) {
+
+                        out.println(message);
+                        out.flush();
+                    }else {
+                        out.println(STOP_STRING);
+                        out.flush();
+                        System.exit(0);
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        });
+        Thread t2 = new Thread(()->{
+
+            while (true) {
+                String message = in.nextLine();
+               if(!message.equals(STOP_STRING)){
+                   System.out.println("Server: " + message);
+               }else System.exit(0);
+            }
+        });
+        t1.start();
+        t2.start();
+    }
 
 
 }
--- src/com/github/vedeshkin/Server.java
+++ src/com/github/vedeshkin/Server.java
@@ -1,6 +1,8 @@
 package com.github.vedeshkin;
 
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
@@ -8,9 +10,70 @@ public class Server {
     private static final int SERVER_PORT = 8089;
     private Scanner in;
     private PrintWriter out;
+    private static Server instance = null;
+    private static final String STOP_STRING = "bye";
 
+    private Server() {
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Server s = Server.getInstance();
+        s.init();
+
+    }
+
+    static Server getInstance() {
+        if (instance == null) instance = new Server();
+        return instance;
+    }
+
+    private void init() {
+
+        Socket s = null;
+        ServerSocket serverSocket;
+
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+            s = serverSocket.accept();
+            in = new Scanner(s.getInputStream());
+            out = new PrintWriter(s.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Thread input = new Thread(() -> {
+
+            while (true) {
+                String message = in.nextLine();
+                if(!message.equals(STOP_STRING)){
+                    System.out.println("Client: " + message);
+                }else System.exit(0);
+            }
+        });
+        Thread output = new Thread(() -> {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+
+                while (true) {
+                    String message = br.readLine();
+                    if (!message.equals(STOP_STRING)) {
+
+                        out.println(message);
+                        out.flush();
+                    }else {
+                        out.println(STOP_STRING);
+                        out.flush();
+                        System.exit(0);
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        input.start();
+        output.start();
+
     }
 }
